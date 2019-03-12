b8485b16076a613277e55c4e1087d70b1feeb6dc
MINT <mint@inslens.com>
2019-03-12 23:47:00 +0800

    wrong alert widget

--- src/components/analysis/block/alert/WrongAlert.vue
+++ src/components/analysis/block/alert/WrongAlert.vue
@@ -6,7 +6,7 @@
         <v-layout column align-start>
           <div class="body-2 font-weigth-bold mb-1">误报率</div>
           <div>
-            <span class="display-3 font-weight-bold">60</span>
+            <span class="display-3 font-weight-bold">{{wrongAlertRate}}</span>
             <strong>&nbsp;&nbsp;%</strong>
           </div>
         </v-layout>
@@ -17,7 +17,7 @@
           :line-width="3"
           padding="15"
           color="white"
-          :value="heartbeats"
+          :value="wrongAlertArr"
           auto-draw
           stroke-linecap="round"
         ></v-sparkline>
@@ -27,11 +27,48 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   data() {
     return {
-      heartbeats: [0, 1, 3, 4, 2, 5, 12, 3, 5, 1]
+      wrongAlertRate: 0,
+      wrongAlertArr: []
     };
+  },
+  methods: {
+    getWrongAlertRate(e) {
+      var wrongAlertCount = 0;
+      var alertCount = 0;
+
+      var alertStateArr = JSON.parse(e.alertState);
+      wrongAlertCount = alertStateArr[2];
+
+      var alertTypeArr = JSON.parse(e.alertType);
+      for (let i = 0; i < alertTypeArr.length; i++) {
+        alertCount += alertTypeArr[i];
+      }
+      return (wrongAlertCount / (alertCount + 1)).toFixed(2);
+    },
+    getWrongAlertTrend() {
+      for (let i = 0; i < this.organizationStatistic.length; i++) {
+        console.log(this.getWrongAlertRate(this.organizationStatistic[i]));
+        // * 100 because vuetify only support int
+        this.wrongAlertArr.unshift(
+          100 * this.getWrongAlertRate(this.organizationStatistic[i])
+        );
+      }
+      console.log(this.wrongAlertArr);
+    }
+  },
+  computed: {
+    ...mapGetters({
+      organizationStatistic: "organization/organizationStatistic"
+    })
+  },
+  mounted() {
+    this.wrongAlertRate = this.getWrongAlertRate(this.organizationStatistic[0]);
+    this.getWrongAlertTrend();
   }
 };
 </script>
